c8e27f866e578cdf681b856fcac80a7629f39574
Mark Dalgleish <mark.john.dalgleish@gmail.com>
2014-09-23 07:25:49 +1000

    Updates

--- build/build.js
+++ build/build.js
@@ -6,7 +6,7 @@ var bespoke = require('bespoke'),
   bullets = require('bespoke-bullets'),
   scale = require('bespoke-scale'),
   progress = require('bespoke-progress'),
-  state = require('bespoke-state');
+  backdrop = require('bespoke-backdrop');
 
 bespoke.from('article', [
   cube(),
@@ -15,16 +15,16 @@ bespoke.from('article', [
   bullets('li, .bullet'),
   scale(),
   progress(),
-  state()
+  backdrop()
 ]);
 
-},{"../../../lib/bespoke-theme-cube.js":2,"bespoke":10,"bespoke-bullets":3,"bespoke-keys":5,"bespoke-progress":6,"bespoke-scale":7,"bespoke-state":8,"bespoke-touch":9}],2:[function(require,module,exports){
+},{"../../../lib/bespoke-theme-cube.js":2,"bespoke":10,"bespoke-backdrop":3,"bespoke-bullets":4,"bespoke-keys":6,"bespoke-progress":7,"bespoke-scale":8,"bespoke-touch":9}],2:[function(require,module,exports){
 
 var classes = require('bespoke-classes');
 var insertCss = require('insert-css');
 
 module.exports = function() {
-  var css = "*{-moz-box-sizing:border-box;box-sizing:border-box;margin:0;padding:0}@media print{*{-webkit-print-color-adjust:exact}}@page{size:landscape;margin:0}.bespoke-parent{-webkit-transition:background .6s ease;transition:background .6s ease;position:absolute;top:0;bottom:0;left:0;right:0;overflow:hidden;-webkit-perspective:600px;perspective:600px}@media print{.bespoke-parent{overflow:visible;position:static}}.bespoke-slide{-webkit-transition:-webkit-transform .6s ease,opacity .6s ease,background .6s ease;transition:transform .6s ease,opacity .6s ease,background .6s ease;-webkit-transform-origin:50% 50% 0;transform-origin:50% 50% 0;-webkit-backface-visibility:hidden;backface-visibility:hidden;display:-webkit-box;display:-webkit-flex;display:-ms-flexbox;display:flex;-webkit-box-orient:vertical;-webkit-box-direction:normal;-webkit-flex-direction:column;-ms-flex-direction:column;flex-direction:column;-webkit-box-pack:center;-webkit-justify-content:center;-ms-flex-pack:center;justify-content:center;-webkit-box-align:center;-webkit-align-items:center;-ms-flex-align:center;align-items:center;text-align:center;width:640px;height:480px;position:absolute;top:50%;margin-top:-240px;left:50%;margin-left:-320px;background:#eaeaea;padding:40px;border-radius:0}@media print{.bespoke-slide{zoom:1!important;height:743px;width:100%;page-break-before:always;position:static;margin:0;-webkit-transition:none;transition:none}}.bespoke-before{-webkit-transform:translateX(100px)translateX(-320px)rotateY(-90deg)translateX(-320px);transform:translateX(100px)translateX(-320px)rotateY(-90deg)translateX(-320px)}@media print{.bespoke-before{-webkit-transform:none;transform:none}}.bespoke-after{-webkit-transform:translateX(-100px)translateX(320px)rotateY(90deg)translateX(320px);transform:translateX(-100px)translateX(320px)rotateY(90deg)translateX(320px)}@media print{.bespoke-after{-webkit-transform:none;transform:none}}.bespoke-inactive{opacity:0;pointer-events:none}@media print{.bespoke-inactive{opacity:1}}.bespoke-active{opacity:1}.bespoke-bullet{-webkit-transition:all .3s ease;transition:all .3s ease}@media print{.bespoke-bullet{-webkit-transition:none;transition:none}}.bespoke-bullet-inactive{opacity:0}li.bespoke-bullet-inactive{-webkit-transform:translateX(16px);transform:translateX(16px)}@media print{li.bespoke-bullet-inactive{-webkit-transform:none;transform:none}}@media print{.bespoke-bullet-inactive{opacity:1}}.bespoke-bullet-active{opacity:1}.bespoke-scale-parent{-webkit-perspective:600px;perspective:600px;position:absolute;top:0;left:0;right:0;bottom:0;pointer-events:none}.bespoke-scale-parent .bespoke-active{pointer-events:auto}@media print{.bespoke-scale-parent{-webkit-transform:none!important;transform:none!important}}.bespoke-progress-parent{position:absolute;top:0;left:0;right:0;height:2px}@media only screen and (min-width:1366px){.bespoke-progress-parent{height:4px}}@media print{.bespoke-progress-parent{display:none}}.bespoke-progress-bar{-webkit-transition:width .6s ease;transition:width .6s ease;position:absolute;height:100%;background:#0089f3;border-radius:0 4px 4px 0}.emphatic,.emphatic .bespoke-slide{background:#eaeaea}pre{padding:26px!important;border-radius:8px}body{font-family:helvetica,arial,sans-serif;font-size:18px;color:#404040}h1{font-size:72px;line-height:82px;letter-spacing:-2px;margin-bottom:16px}h2{font-size:42px;letter-spacing:-1px;margin-bottom:8px}h3{font-size:24px;font-weight:400;margin-bottom:24px;color:#606060}hr{visibility:hidden;height:20px}ul{list-style:none}li{margin-bottom:12px}p{margin:0 100px 12px;line-height:22px}a{color:#0089f3;text-decoration:none}";
+  var css = "*{-moz-box-sizing:border-box;box-sizing:border-box;margin:0;padding:0}@media print{*{-webkit-print-color-adjust:exact}}@page{size:landscape;margin:0}.bespoke-parent{-webkit-transition:background .6s ease;transition:background .6s ease;position:absolute;top:0;bottom:0;left:0;right:0;overflow:hidden;-webkit-perspective:600px;perspective:600px}@media print{.bespoke-parent{overflow:visible;position:static}}.bespoke-slide{-webkit-transition:-webkit-transform .6s ease,opacity .6s ease,background .6s ease;transition:transform .6s ease,opacity .6s ease,background .6s ease;-webkit-transform-origin:50% 50% 0;transform-origin:50% 50% 0;-webkit-backface-visibility:hidden;backface-visibility:hidden;display:-webkit-box;display:-webkit-flex;display:-ms-flexbox;display:flex;-webkit-box-orient:vertical;-webkit-box-direction:normal;-webkit-flex-direction:column;-ms-flex-direction:column;flex-direction:column;-webkit-box-pack:center;-webkit-justify-content:center;-ms-flex-pack:center;justify-content:center;-webkit-box-align:center;-webkit-align-items:center;-ms-flex-align:center;align-items:center;text-align:center;width:640px;height:480px;position:absolute;top:50%;margin-top:-240px;left:50%;margin-left:-320px;background:#eaeaea;padding:40px;border-radius:0}@media print{.bespoke-slide{zoom:1!important;height:743px;width:100%;page-break-before:always;position:static;margin:0;-webkit-transition:none;transition:none}}.bespoke-before{-webkit-transform:translateX(100px)translateX(-320px)rotateY(-90deg)translateX(-320px);transform:translateX(100px)translateX(-320px)rotateY(-90deg)translateX(-320px)}@media print{.bespoke-before{-webkit-transform:none;transform:none}}.bespoke-after{-webkit-transform:translateX(-100px)translateX(320px)rotateY(90deg)translateX(320px);transform:translateX(-100px)translateX(320px)rotateY(90deg)translateX(320px)}@media print{.bespoke-after{-webkit-transform:none;transform:none}}.bespoke-inactive{opacity:0;pointer-events:none}@media print{.bespoke-inactive{opacity:1}}.bespoke-active{opacity:1}.bespoke-bullet{-webkit-transition:all .3s ease;transition:all .3s ease}@media print{.bespoke-bullet{-webkit-transition:none;transition:none}}.bespoke-bullet-inactive{opacity:0}li.bespoke-bullet-inactive{-webkit-transform:translateX(16px);transform:translateX(16px)}@media print{li.bespoke-bullet-inactive{-webkit-transform:none;transform:none}}@media print{.bespoke-bullet-inactive{opacity:1}}.bespoke-bullet-active{opacity:1}.bespoke-scale-parent{-webkit-perspective:600px;perspective:600px;position:absolute;top:0;left:0;right:0;bottom:0;pointer-events:none}.bespoke-scale-parent .bespoke-active{pointer-events:auto}@media print{.bespoke-scale-parent{-webkit-transform:none!important;transform:none!important}}.bespoke-progress-parent{position:absolute;top:0;left:0;right:0;height:2px}@media only screen and (min-width:1366px){.bespoke-progress-parent{height:4px}}@media print{.bespoke-progress-parent{display:none}}.bespoke-progress-bar{-webkit-transition:width .6s ease;transition:width .6s ease;position:absolute;height:100%;background:#0089f3;border-radius:0 4px 4px 0}.emphatic{background:#eaeaea}.bespoke-backdrop{position:absolute;top:0;left:0;right:0;bottom:0;-webkit-transform:translateZ(0);transform:translateZ(0);-webkit-transition:opacity .6s ease;transition:opacity .6s ease;opacity:0;z-index:-1}.bespoke-backdrop-active{opacity:1}pre{padding:26px!important;border-radius:8px}body{font-family:helvetica,arial,sans-serif;font-size:18px;color:#404040}h1{font-size:72px;line-height:82px;letter-spacing:-2px;margin-bottom:16px}h2{font-size:42px;letter-spacing:-1px;margin-bottom:8px}h3{font-size:24px;font-weight:400;margin-bottom:24px;color:#606060}hr{visibility:hidden;height:20px}ul{list-style:none}li{margin-bottom:12px}p{margin:0 100px 12px;line-height:22px}a{color:#0089f3;text-decoration:none}";
   insertCss(css, { prepend: true });
 
   return function(deck) {
@@ -32,7 +32,60 @@ module.exports = function() {
   };
 };
 
-},{"bespoke-classes":4,"insert-css":11}],3:[function(require,module,exports){
+},{"bespoke-classes":5,"insert-css":11}],3:[function(require,module,exports){
+module.exports = function() {
+  return function(deck) {
+    var backdrops;
+
+    function createBackdropForSlide(slide) {
+      var backdropAttribute = slide.getAttribute('data-bespoke-backdrop');
+
+      if (backdropAttribute) {
+        var backdrop = document.createElement('div');
+        backdrop.className = backdropAttribute;
+        backdrop.classList.add('bespoke-backdrop');
+        deck.parent.appendChild(backdrop);
+        return backdrop;
+      }
+    }
+
+    function updateClasses(el) {
+      if (el) {
+        var index = backdrops.indexOf(el),
+          currentIndex = deck.slide();
+
+        removeClass(el, 'active');
+        removeClass(el, 'inactive');
+        removeClass(el, 'before');
+        removeClass(el, 'after');
+
+        if (index !== currentIndex) {
+          addClass(el, 'inactive');
+          addClass(el, index < currentIndex ? 'before' : 'after');
+        } else {
+          addClass(el, 'active');
+        }
+      }
+    }
+
+    function removeClass(el, className) {
+      el.classList.remove('bespoke-backdrop-' + className);
+    }
+
+    function addClass(el, className) {
+      el.classList.add('bespoke-backdrop-' + className);
+    }
+
+    backdrops = deck.slides
+      .map(createBackdropForSlide);
+
+    deck.on('activate', function() {
+      backdrops.forEach(updateClasses);
+    });
+  };
+};
+
+},{}],4:[function(require,module,exports){
 module.exports = function(options) {
   return function(deck) {
     var activeSlideIndex,
@@ -79,6 +132,12 @@ module.exports = function(options) {
               bullet.classList.add('bespoke-bullet-inactive');
               bullet.classList.remove('bespoke-bullet-active');
             }
+
+            if (s === slideIndex && b === bulletIndex) {
+              bullet.classList.add('bespoke-bullet-current');
+            } else {
+              bullet.classList.remove('bespoke-bullet-current');
+            }
           });
         });
       },
@@ -98,7 +157,7 @@ module.exports = function(options) {
   };
 };
 
-},{}],4:[function(require,module,exports){
+},{}],5:[function(require,module,exports){
 module.exports = function() {
   return function(deck) {
     var addClass = function(el, cls) {
@@ -134,7 +193,7 @@ module.exports = function() {
   };
 };
 
-},{}],5:[function(require,module,exports){
+},{}],6:[function(require,module,exports){
 module.exports = function(options) {
   return function(deck) {
     var isHorizontal = options !== 'vertical';
@@ -154,7 +213,7 @@ module.exports = function(options) {
   };
 };
 
-},{}],6:[function(require,module,exports){
+},{}],7:[function(require,module,exports){
 module.exports = function(options) {
   return function (deck) {
     var progressParent = document.createElement('div'),
@@ -172,7 +231,7 @@ module.exports = function(options) {
   };
 };
 
-},{}],7:[function(require,module,exports){
+},{}],8:[function(require,module,exports){
 module.exports = function(options) {
   return function(deck) {
     var parent = deck.parent,
@@ -219,24 +278,6 @@ module.exports = function(options) {
 
 };
 
-},{}],8:[function(require,module,exports){
-module.exports = function() {
-  return function(deck) {
-    var modifyState = function(method, event) {
-      var attr = event.slide.getAttribute('data-bespoke-state');
-
-      if (attr) {
-        attr.split(' ').forEach(function(state) {
-          deck.parent.classList[method](state);
-        });
-      }
-    };
-
-    deck.on('activate', modifyState.bind(null, 'add'));
-    deck.on('deactivate', modifyState.bind(null, 'remove'));
-  };
-};
-
 },{}],9:[function(require,module,exports){
 module.exports = function(options) {
   return function(deck) {
@@ -267,130 +308,81 @@ module.exports = function(options) {
 };
 
 },{}],10:[function(require,module,exports){
-var decks = [],
+var from = function(selectorOrElement, plugins) {
+  var parent = selectorOrElement.nodeType === 1 ? selectorOrElement : document.querySelector(selectorOrElement),
+    slides = [].filter.call(parent.children, function(el) { return el.nodeName !== 'SCRIPT'; }),
+    activeSlide = slides[0],
+    listeners = {},
 
-	addClass = function(el, cls) {
-		el.classList.add('bespoke-' + cls);
-	},
+    activate = function(index, customData) {
+      if (!slides[index]) {
+        return;
+      }
 
-	removeClass = function(el, cls) {
-		el.className = el.className
-			.replace(RegExp('bespoke-' + cls +'(\\s|$)', 'g'), ' ')
-			.trim();
-	},
+      fire('deactivate', createEventData(activeSlide, customData));
+      activeSlide = slides[index];
+      fire('activate', createEventData(activeSlide, customData));
+    },
 
-	from = function(selectorOrElement, plugins) {
-		var parent = selectorOrElement.nodeType === 1 ? selectorOrElement : document.querySelector(selectorOrElement),
-			slides = [].filter.call(parent.children, function(el) { return el.nodeName !== 'SCRIPT'; }),
-			activeSlide = slides[0],
-			listeners = {},
+    slide = function(index, customData) {
+      if (arguments.length) {
+        fire('slide', createEventData(slides[index], customData)) && activate(index, customData);
+      } else {
+        return slides.indexOf(activeSlide);
+      }
+    },
 
-			activate = function(index, customData) {
-				if (!slides[index]) {
-					return;
-				}
+    step = function(offset, customData) {
+      var slideIndex = slides.indexOf(activeSlide) + offset;
 
-				fire('deactivate', createEventData(activeSlide, customData));
+      fire(offset > 0 ? 'next' : 'prev', createEventData(activeSlide, customData)) && activate(slideIndex, customData);
+    },
 
-				activeSlide = slides[index];
+    on = function(eventName, callback) {
+      (listeners[eventName] || (listeners[eventName] = [])).push(callback);
 
-				slides.map(deactivate);
+      return function() {
+        listeners[eventName] = listeners[eventName].filter(function(listener) {
+          return listener !== callback;
+        });
+      };
+    },
 
-				fire('activate', createEventData(activeSlide, customData));
+    fire = function(eventName, eventData) {
+      return (listeners[eventName] || [])
+        .reduce(function(notCancelled, callback) {
+          return notCancelled && callback(eventData) !== false;
+        }, true);
+    },
 
-				addClass(activeSlide, 'active');
-				removeClass(activeSlide, 'inactive');
-			},
+    createEventData = function(el, eventData) {
+      eventData = eventData || {};
+      eventData.index = slides.indexOf(el);
+      eventData.slide = el;
+      return eventData;
+    },
 
-			deactivate = function(el, index) {
-				var offset = index - slides.indexOf(activeSlide),
-					offsetClass = offset > 0 ? 'after' : 'before';
+    deck = {
+      on: on,
+      fire: fire,
+      slide: slide,
+      next: step.bind(null, 1),
+      prev: step.bind(null, -1),
+      parent: parent,
+      slides: slides
+    };
 
-				['before(-\\d+)?', 'after(-\\d+)?', 'active', 'inactive'].map(removeClass.bind(null, el));
+  (plugins || []).forEach(function(plugin) {
+    plugin(deck);
+  });
 
-				el !== activeSlide &&
-					['inactive', offsetClass, offsetClass + '-' + Math.abs(offset)].map(addClass.bind(null, el));
-			},
+  activate(0);
 
-			slide = function(index, customData) {
-				if (arguments.length) {
-					fire('slide', createEventData(slides[index], customData)) && activate(index, customData);
-				} else {
-					return slides.indexOf(activeSlide);
-				}
-			},
-
-			step = function(offset, customData) {
-				var slideIndex = slides.indexOf(activeSlide) + offset;
-
-				fire(offset > 0 ? 'next' : 'prev', createEventData(activeSlide, customData)) && activate(slideIndex, customData);
-			},
-
-			on = function(eventName, callback) {
-				(listeners[eventName] || (listeners[eventName] = [])).push(callback);
-
-				return function() {
-					listeners[eventName] = listeners[eventName].filter(function(listener) {
-						return listener !== callback;
-					});
-				};
-			},
-
-			fire = function(eventName, eventData) {
-				return (listeners[eventName] || [])
-					.reduce(function(notCancelled, callback) {
-						return notCancelled && callback(eventData) !== false;
-					}, true);
-			},
-
-			createEventData = function(el, eventData) {
-				eventData = eventData || {};
-				eventData.index = slides.indexOf(el);
-				eventData.slide = el;
-				return eventData;
-			},
-
-			deck = {
-				on: on,
-				fire: fire,
-				slide: slide,
-				next: step.bind(null, 1),
-				prev: step.bind(null, -1),
-				parent: parent,
-				slides: slides
-			};
-
-		addClass(parent, 'parent');
-
-		slides.map(function(el) {
-			addClass(el, 'slide');
-		});
-
-		(plugins || []).forEach(function(plugin) {
-			plugin(deck);
-		});
-
-		activate(0);
-
-		decks.push(deck);
-
-		return deck;
-	},
-
-	callOnAllDecks = function(method) {
-		return function() {
-			var args = arguments;
-			decks.map(function(deck) {
-				deck[method].apply(null, args);
-			});
-		};
-	};
+  return deck;
+};
 
 module.exports = {
-	from: from,
-	slide: callOnAllDecks('slide'),
-	next: callOnAllDecks('next'),
-	prev: callOnAllDecks('prev')
+  from: from
 };
 
 },{}],11:[function(require,module,exports){
